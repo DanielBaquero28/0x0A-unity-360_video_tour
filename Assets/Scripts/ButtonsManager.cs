ec39358b0ed3f343acab276bde074549f3a0dd73
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-08-21 21:14:39 -0500

    [Feature]: Other InfoBoxes and music remaining

---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class ButtonsManager : MonoBehaviour
+{
+    //public GameObject cam;
+
+    public GameObject livingRoom;
+    public GameObject cantina;
+    public GameObject cube;
+    public GameObject mezzanine;
+
+    public GameObject canvas;
+
+    public GameObject infoBox;
+
+    GameObject livingRoomCanvas;
+    GameObject cantinaCanvas;
+    GameObject cubeCanvas;
+    GameObject mezzanineCanvas;
+
+    public void Start()
+    {
+        livingRoomCanvas = canvas.transform.GetChild(0).gameObject;
+        cantinaCanvas = canvas.transform.GetChild(1).gameObject;
+        cubeCanvas = canvas.transform.GetChild(2).gameObject;
+        mezzanineCanvas = canvas.transform.GetChild(3).gameObject;
+    }
+
+
+    public void ToCantina()
+    {
+        //Debug.Log("ToCantina");
+        livingRoom.SetActive(false);
+        cantina.SetActive(true);
+        cube.SetActive(false);
+        livingRoomCanvas.SetActive(false);
+        cantinaCanvas.SetActive(true);
+        cubeCanvas.SetActive(false);
+        infoBox.SetActive(false);
+    }
+
+    public void ToLivingRoom()
+    {
+        //Debug.Log("ToLivingRoom");
+        livingRoom.SetActive(true);
+        cantina.SetActive(false);
+        cube.SetActive(false);
+        cubeCanvas.SetActive(false);
+        livingRoomCanvas.SetActive(true);
+        cantinaCanvas.SetActive(false);
+    }
+
+    public void ToCube()
+    {
+        //Debug.Log("ToCube");
+        livingRoom.SetActive(false);
+        cantina.SetActive(false);
+        cube.SetActive(true);
+        mezzanine.SetActive(false);
+        livingRoomCanvas.SetActive(false);
+        cantinaCanvas.SetActive(false);
+        cubeCanvas.SetActive(true);
+        mezzanineCanvas.SetActive(false);
+        infoBox.SetActive(false);
+    }
+
+    public void ToMezzanine()
+    {
+        //Debug.Log("ToMezzanine");
+        cube.SetActive(false);
+        mezzanine.SetActive(true);
+        cubeCanvas.SetActive(false);
+        mezzanineCanvas.SetActive(true);
+    }
+
+    public void InfoBox()
+    {
+        if (!infoBox.activeSelf)
+        {
+            infoBox.SetActive(true);
+        }
+        else
+        {
+            infoBox.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/LaserInput.cs
@@ -0,0 +1,43 @@
+﻿using Valve.VR;
+using Valve.VR.Extras;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+public class LaserInput : MonoBehaviour
+{
+    public static GameObject currentObject;
+    int currentID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = null;
+        currentID = 0;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            int id = hit.collider.gameObject.GetInstanceID();
+
+            if (currentID != id)
+            {
+                currentID = id;
+                currentObject = hit.collider.gameObject;
+
+                string tag = currentObject.tag;
+                if (tag == "UI Element")
+                {
+                    Debug.Log("Touched UI Element");
+                }
+
+            }
+        }
+    }
+}
